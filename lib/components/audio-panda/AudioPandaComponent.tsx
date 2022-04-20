8274a1fe4f0e94bb26d391bd2ad6c25c7a440411
fransmpldv <fran@smpldv.com>
2022-04-20 15:38:16 -0500

    audio_panda 1.1 fran

--- lib/components/audio-panda/AudioPandaComponent.tsx
+++ lib/components/audio-panda/AudioPandaComponent.tsx
@@ -12,16 +12,6 @@ export default function App() {
     artboard: "Panda Teddy",
     layout: new Layout({ fit: Fit.Contain }),
   });
-// button style goes here
-const Button = styled.button`
-  background-color: black;
-  color: white;
-  font-size: 20px;
-  padding: 10px 60px;
-  border-radius: 5px;
-  margin: 10px 0px;
-  cursor: pointer;
-`;
 
 
 

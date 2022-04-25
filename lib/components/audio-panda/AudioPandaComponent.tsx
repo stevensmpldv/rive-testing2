f46ca68dfe261e528685e5d406cd8d116404e941
stevensmpldv <steven@smpldv.com>
2022-04-25 15:26:58 -0500

    frans version

--- lib/components/audio-panda/AudioPandaComponent.tsx
+++ lib/components/audio-panda/AudioPandaComponent.tsx
@@ -1,78 +1,140 @@
-import { useState, useEffect } from "react";
-import { useRive, Layout, Fit } from "rive-react";
+import React from "react";
+import { useRive, useStateMachineInput, Layout, Fit } from "rive-react";
+import styled from 'styled-components';
+//themes
+const theme = {
+    blue: {
+        default: '#3498DB',
+        hover:  '#2874A6'
+    }
+    red: {
+        default: '#E74C3C',
+        hover: '#B03A2E'
+    }
+    green: {
+        default: '#2ECC71',
+        hover: '#239B56'
+    }
+}
+//button styling 
+const Button = styled.button`
+    background-color: ${props => theme[props.theme].default};
+    color: white;
+    padding: 5px 15px;
+    border-radius: 5px;
+    outline: 0;
+    text-transform: uppercase;
+    margin: 10px 0px;
+    cursor: pointer;
+    box-shadow: 0px 2px 2px lightgray;
+    transition: ease background-color: 250ms;
+    &:hover {
+        background-color: ${props => theme[props.theme].hover}
+    }
+`
+Button.defaultProps = {
 
+    theme: 'blue'
+}
+
+
+
+// animation constants
 export default function App() {
-  const [truckButtonText, setTruckButtonText] = useState("Start Truck");
-  const [wiperButtonText, setWiperButtonText] = useState("Start Wipers");
+    const STATEMACHINE = "Login Machine"
+    const LISTENING = "isListening"
+    const BLINK = "trigBlink"
+    const FOOTTAP = "trigFoottap"
+    const SUCCESS = "trigSuccess"
+    const FAIL = "trigFail";
+    
 
-  // animation will show the first frame but not start playing
-  const { rive, RiveComponent } = useRive({
-    src: "audio_panda_teddy_02.riv",
-    artboard: "Panda Teddy",
-    layout: new Layout({ fit: Fit.Contain }),
-  });
 
-  useEffect(() => {
-    if (rive) {
-      // Listen for play events to update button text
-      rive.on("play", (event) => {
-        const names = event.data;
-        names.forEach((name) => {
-          if (name === "idle") {
-            setTruckButtonText("Stop Truck");
-          } else if (name === "success") {
-            setWiperButtonText("Stop Wipers");
-          }
-        });
-      });
 
-      // Listen for pause events to update button text
-      rive.on("pause", (event) => {
-        const names = event.data;
-        names.forEach((name) => {
-          if (name === "idle") {
-            setTruckButtonText("Start Truck");
-          } else if (name === "success") {
-            setWiperButtonText("Start Wipers");
-          }
-        });
-      });
-    }
-  }, [rive]);
+//load panda frame image and auto play idle
+    const { rive, RiveComponent } = useRive({
+        src: "audio_panda_teddy_04.riv",
+        stateMachines: "Login Machine",
+        autoplay: true,
+        layout: new Layout({ fit: Fit.Contain }),
 
-  function onStartTruckClick() {
-    if (rive) {
-      if (rive.playingAnimationNames.includes("idle")) {
-        rive.pause("idle");
-      } else {
-        rive.play("idle");
-      }
-    }
-  }
+    });
+// state machine constants
+    const BLINKB = useStateMachineInput( 
+        rive, 
+        STATEMACHINE, 
+        BLINK 
+        );
+    const FOOTTAPB = useStateMachineInput( 
+        rive, 
+        STATEMACHINE, 
+        FOOTTAP 
+            );
+    const SUCCESSB = useStateMachineInput( 
+        rive, 
+        STATEMACHINE, 
+        SUCCESS 
+                );
+    const FAILB = useStateMachineInput( 
+        rive, 
+        STATEMACHINE, 
+        FAIL 
+        );
+    const LISTENINGB = useStateMachineInput( 
+        rive, 
+        STATEMACHINE, 
+        LISTENING
+            );
 
-  function onStartWiperClick() {
-    if (rive) {
-      if (rive.playingAnimationNames.includes("success")) {
-        rive.pause("success");
-      } else {
-        rive.play("success");
-      }
-    }
-  }
 
-  return (
-    <>
-      <div>
-        <RiveComponent style={{ height: "1000px" }} />
-      </div>
-      <div>
-        <button id="idle" onClick={onStartTruckClick}>
-          {truckButtonText}
-        </button>
-        <button id="wipers" onClick={onStartWiperClick}>
-          {wiperButtonText}
-        </button>
-      </div>
+//logger
+if(rive){
+    console.log(rive.contents);
+}
+
+ return (
+     <>
+ <div>
+    <Button theme="red"  onClick={() => BLINKB.fire}>
+        Blink
+    </Button>
+ </div>
+
+ <div>
+    <Button theme="red"  onClick={() => FOOTTAPB.fire}>
+        Tap Foot
+    </Button>
+ </div>
+
+ <div>
+    <Button theme="red"  onClick={() => SUCCESSB.fire}>
+        Nod Head
+    </Button>
+ </div>
+
+ <div>
+    <Button theme="red"  onClick={() => FAILB.fire}>
+        Shake Head
+    </Button>
+ </div>
+
+ <div>
+ <Button theme="blue"  onClick={() => LISTENINGB.value = true}>
+     Listen
+ </Button>
+</div>
+
+<div>
+ <Button theme="green"  onClick={() => LISTENINGB.value = false}>
+     Idle
+ </Button>
+</div>
+
     </>
+<<<<<<< Updated upstream
   );
+=======
+ );
+
+>>>>>>> Stashed changes
 }

890fd0218b240bc9f41a44adf06ae244a2c52db3
stevensmpldv <steven@smpldv.com>
2022-05-03 12:50:56 -0500

    updated audiopanda

--- lib/components/audio-panda/AudioPandaComponent.tsx
+++ lib/components/audio-panda/AudioPandaComponent.tsx
@@ -53,7 +53,7 @@ export default function App() {
 
 //load panda frame image and auto play idle
     const { rive, RiveComponent } = useRive({
-        src: "audio_panda_teddy_07.riv",
+        src: "audio_panda_teddy_08.riv",
         stateMachines: STATEMACHINE,
         autoplay: true,
         layout: new Layout({ fit: Fit.Contain }),
